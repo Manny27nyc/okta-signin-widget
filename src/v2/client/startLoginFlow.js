c4f5d8cc2df7f47951add94bdb7624cffacc811f
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2021-11-05 11:53:08 -0400

    pc: moved feature logic to new file

--- src/v2/client/startLoginFlow.js
+++ src/v2/client/startLoginFlow.js
@@ -11,16 +11,9 @@
  */
 
 import Errors from 'util/Errors';
-import Logger from 'util/Logger';
-import Enums from 'util/Enums';
 import { interact } from './interact';
 import { introspect } from './introspect';
 import sessionStorageHelper from './sessionStorageHelper';
-import {
-  getTransactionMeta,
-  saveTransactionMeta,
-  clearTransactionMeta,
-} from './transactionMeta';
 
 const handleProxyIdxResponse = async (settings) => {
   return Promise.resolve({
@@ -29,90 +22,6 @@ const handleProxyIdxResponse = async (settings) => {
     neededToProceed: [],
   });
 };
-
-async function startSpecificIdxFlow(originalIdxResp, flow='') {
-  let idxResponse = originalIdxResp;
-
-  try {
-    if (flow === Enums.LOGIN_FLOW) {
-      // default IDX response from interact is "Login" page/flow. Do nothing
-    }
-    else if (flow === Enums.REGISTRATION_FLOW) {
-      idxResponse = await idxResponse.proceed('select-enroll-profile');
-    }
-    else if (flow === Enums.RESET_PASSWORD_FLOW) {
-      // if `currentAuthenticator-recover` action is not available on the parsed idxResponse,
-      // reject an InitialFlowError to be caught in `startInteractionCodeFlow`, this mimics a bad .proceed() call
-      idxResponse = idxResponse.actions['currentAuthenticator-recover'] ?
-        idxResponse.actions['currentAuthenticator-recover']() :
-        Promise.reject(new Errors.InitialFlowError('Unable to invoke desired action', flow));
-    }
-    else if (flow === Enums.UNLOCK_ACCOUNT_FLOW) {
-      // requires: introspect -> identify-recovery -> select-authenticator-unlock-account
-      idxResponse = await idxResponse.proceed('identify-recovery');
-    }
-    else {
-      Logger.warn(`Unknown \`flow\` value: ${flow}`);
-      throw new Errors.InitialFlowError('Unknown flow value', flow);
-    }
-
-    // default to original idx response
-    return idxResponse;
-  }
-  catch (err) {
-    // catches and handles `Unknown remediation` errors thrown okta-idx-js
-    if (typeof err === 'string' && err.startsWith('Unknown remediation choice')) {
-      Logger.warn(`initialFlow [${flow}] not valid with current Org configurations`);
-      throw new Errors.InitialFlowError('Unable to proceed to desired view', flow);
-    }
-    else {
-      // do not catch non-`Unknown remediation` errors here
-      throw err;
-    }
-  }
-}
-
-async function startInteractionCodeFlow(settings) {
-  let idxResponse = await interact(settings);
-
-  try {
-    const configuredFlow = settings.get('initialFlow');
-    if (configuredFlow) {
-      const meta = await getTransactionMeta(settings);
-
-      if (meta.flow !== configuredFlow) {
-        if (meta.flow) {
-          // configured flow and active flow do not match, abandon active flow, start new (configured) flow
-          Logger.warn(`Cancelling current '${meta.flow}' flow to start '${configuredFlow}' flow`);
-          sessionStorageHelper.removeStateHandle();
-          clearTransactionMeta(settings);
-        }
-
-        idxResponse = await startSpecificIdxFlow(idxResponse, configuredFlow);
-        const newMeta = Object.assign({}, {...meta}, {flow: configuredFlow});
-        saveTransactionMeta(settings, newMeta);
-      }
-    }
-  }
-  catch (err) {
-    if (err instanceof Errors.InitialFlowError) {
-      // TODO: add meaningful "error" message, explaining to user why flow was interrupted
-      if (!idxResponse.rawIdxState.messages) {
-        idxResponse.rawIdxState.messages = {type:'array', value: []};
-      }
-      idxResponse.rawIdxState.messages.value.push({message: 'Please contact Jeff'});
-    }
-    else {
-      // do not catch unknown errors here
-      throw err;
-    }
-  }
-  finally {
-    settings.set('initialFlow', false);
-  }
-
-  return idxResponse;
-}
 
 export async function startLoginFlow(settings) {
   // Return a preset response
@@ -126,7 +35,7 @@ export async function startLoginFlow(settings) {
 
   // Use interaction code flow, if enabled
   if (settings.get('useInteractionCodeFlow')) {
-    return startInteractionCodeFlow(settings);
+    return interact(settings);
   }
 
   // Use stateToken from session storage if exists
